567aac5bc6b3dd0c5416dad4d1652ff3b3b29d70
Ad1S1n <arhmnajs@gmail.com>
2025-06-08 10:39:01 +0530

    Practicing React Native

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World!</Text>
       <StatusBar style="auto" />
+      <Box color="red" />
     </View>
   );
 }
@@ -13,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ccc',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+export const Box = (props) => (
+  <View style={{ width: 100, height: 100, backgroundColor: props.color }} />
+);
