ff159206aac7e17138e9d0f6177c5350bc3ca105
Ad1S1n <arhmnajs@gmail.com>
2025-06-14 13:48:36 +0530

    Practicing buttons

--- App.js
+++ App.js
@@ -1,24 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, Button, Image, Pressable } from 'react-native'
+import React from 'react'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-      <Box color="red" />
-    </View>
-  );
+    <SafeAreaView style={{ width:'100%', height:'100%', flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello World!!</Text>
+      <Image
+        source={{ uri: 'https://media.licdn.com/dms/image/v2/C4E0BAQF3QkyXbn7Yyg/company-logo_200_200/company-logo_200_200/0/1630642789117/sakec_logo?e=2147483647&v=beta&t=QHrB2s5p4cuAlKLJT6GiEDjCgWfDeseCE9rkQFP7QNs' }}
+        style={{ width: 100, height: 100 }}
+        resizeMode='contain'
+      />
+      <Button title="Click ME!!" onPress={() => alert('Button Clicked!')} />
+      <Pressable>
+        
+      </Pressable>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ccc',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-export const Box = (props) => (
-  <View style={{ width: 100, height: 100, backgroundColor: props.color }} />
-);
+export default App
